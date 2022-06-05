fd3f8e9569a60b8a42f20e71f99f2202ac129621
santabe04 <sabelle@alumno.huergo.edu.ar>
2022-06-05 18:56:47 -0400

    Modify Controller

--- controller.py
+++ controller.py
@@ -7,4 +7,19 @@ from qr_decode import Decode #Import the Decode class
 class Controller():
     def __init__(self):
         '''Is the initiation from the class'''
-        pass
+        self.View = View() #Creates a new View object
+        self.Model = Model() #Creates a new Model object
+        self.Encode = Encode() #Creates a new Encode object
+        self.Decode = Decode() #Creates a new Decode object
+        self.main() #Runs the main function
+
+    def main(self):
+        '''Is the main loop'''
+        buttonState = "active"
+        while True:
+            messageList = [[0,'Log In'],[1,'Username'],[1,'Password']] #TEST -- Messages to print in screen with the row
+            self.View.login_screen(buttonState) #Calls write_screen from View class
+            test = input('') #TEST -- Is to prevent from closing the program
+
+if __name__ == "__main__":
+    controller = Controller()
